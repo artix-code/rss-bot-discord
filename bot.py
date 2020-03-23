6af94d6be7d812695773aff18066980ab8fdf878
Artik Zenevich <zenewitcha@yandex.by>
2020-03-23 17:25:51 +0300

    aaarrrggghhhh!!!

--- bot.py
+++ bot.py
@@ -4,11 +4,12 @@ import config
 
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!') 
 
 @bot.command(pass_context=True)  
 async def news(ctx): 
-    await ctx.send('Все ссылки: \n' + '\n'.join(feed.get_news(config.NEWS_URL)))
+    # не уверен что это хорошее решение
+    await ctx.send('Все ссылки: \n' + '\n'.join(feed.get_news(config.NEWS_URL))) 
 
 bot.run(config.TOKEN)
 
--- feed.py
+++ feed.py
@@ -1,16 +1,12 @@
 import feedparser
 import config
 
-# парсим RSS
 def get_news(url):
     news_feed = feedparser.parse(url)
     news = []
-    # переменная для все новостей
     entry = news_feed.entries
 
     for x in entry: 
         news.append(x.link) 
 
     return news
-
-get_news(config.NEWS_URL)
