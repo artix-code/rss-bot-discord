e116056c1cf0e9eb3584b221d2c8643346a4dc06
Artik Zenevich <zenewitcha@yandex.by>
2020-03-23 16:51:01 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+import feed
+import config
+
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(pass_context=True)  
+async def news(ctx): 
+    await ctx.send('Все ссылки: \n' + '\n'.join(feed.get_news(config.NEWS_URL)))
+
+bot.run(config.TOKEN)
+
+
+
+
